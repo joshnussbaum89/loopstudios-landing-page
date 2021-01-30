ec58a1be811895f6f4ada7681053f198e2157427
Josh Nussbaum <Joshnussbaum89@gmail.com>
2021-01-30 17:45:11 -0600

    add side nav modal

--- app.js
+++ app.js
@@ -1,14 +1,42 @@
+const sideNav = document.getElementById("myNav");
+
+
+
 /**
- * Toggles Mobile Navigation Menu
- @param {eventTarget} onClick 
+ * Toggles Mobile Navigation Menu On User Click
  ***************************************************************/
 
-function toggleHamburgerMenu(e) {
+const toggleHamburgerMenu = () => {
     const hamburgerTop = document.querySelector('.hamburger-top');
     const hamburgerBottom = document.querySelector('.hamburger-bottom');
     hamburgerTop.classList.toggle('hamburger-x-top');
     hamburgerBottom.classList.toggle('hamburger-x-bottom');
+
+    if (sideNav.classList.contains('close')) {
+        sideNav.style.width = "0%";
+        sideNav.classList.remove('close');
+        sideNav.classList.add('open');
+    } else if (sideNav.classList.contains('open')) {
+        sideNav.style.width = "100%";
+        sideNav.classList.remove('open');
+        sideNav.classList.add('close');
+    }
 }
 
-// Hamburger menu click event
+/**
+ * Toggles Mobile Navigation Menu On Window Resize
+ ***************************************************************/
+
+const windowResizeWindowToggle = () => {
+    let newWidth = window.innerWidth;
+
+    if (newWidth >= 768 && sideNav.classList.contains('close')) {
+        toggleHamburgerMenu();
+    }
+}
+
+// Navigation menu click
 document.querySelector('.hamburger').addEventListener('click', toggleHamburgerMenu);
+
+// Navigation menu window resize
+window.addEventListener('resize', windowResizeWindowToggle);
